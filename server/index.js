9c5ff25aeb4bbc158b60ed06f9def0455b0b2ce9
cptran777 <cptran14@gmail.com>
2016-10-07 16:01:59 -0700

    add route to index set up index html and start a style sheet for custom styles

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 
 let express = require('express');
 let bodyparser = require('body-parser');
+let path = require('path');
 
 let app = express();
 
@@ -17,6 +18,12 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyparser.json());
+
+/********************* INIT ROUTES *************************/
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+});
 
 /********************* INIT SERVER *************************/
 
